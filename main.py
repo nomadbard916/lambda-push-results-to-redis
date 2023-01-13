58a26ea1b32db2344e064a020a17e95d1bb08fe2
kevin-hou-cf <kevin.hou@clickforce.com.tw>
2023-01-13 15:00:47 +0800

    implement with pipeline

--- main.py
+++ main.py
@@ -12,18 +12,23 @@ def lambda_handler(event, context):
     key = event["Records"][0]["s3"]["object"]["key"]
 
     # TODO: pipeline
-    # Connect to redis
+    # Connect to redis and set pipeline
     redis_client = redis.Redis(
         host="your_redis_host", port=6379, password=REDIS_PASSWORD
     )
+    redis_pipeline = redis_client.pipeline()
 
     # download the CSV file from S3
     s3 = boto3.client("s3")
     obj = s3.get_object(Bucket=bucket, Key=key)
+
     csv_file = obj["Body"].read()
 
     # parse the CSV file
     rows = csv.DictReader(csv_file.decode().splitlines())
     for row in rows:
         # set the Redis key-value
-        redis_client.set(row["key"], row["value"])
+        # TODO: TTL by different models
+        redis_pipeline.set(row["key"], row["value"], 3600)
+
+    redis_pipeline.execute()
