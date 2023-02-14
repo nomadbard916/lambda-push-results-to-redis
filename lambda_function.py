57ae43a0062c0b4825040efa4084672999f9dde4
kevin-hou-cf <kevin.hou@clickforce.com.tw>
2023-02-14 17:40:00 +0800

    [refactor] extract methods to make code look betterˆ

--- lambda_function.py
+++ lambda_function.py
@@ -17,20 +17,9 @@ TTL = 86400
 def lambda_handler(event, context):
     # TODO: sanity check for data contents
 
-    # extract the S3 bucket and object key from the event
-    s3_event_data = event["Records"][0]["s3"]
-    bucket = s3_event_data["bucket"]["name"]
-    key = s3_event_data["object"]["key"]
+    bucket, key = extract_s3_params(event)
 
-    # download the CSV file from S3 and parse to iterator of lists
-    if IS_DEV:
-        # make bytes ourselves, by reading local file directly,
-        # skipping event data altogether.
-        with open("example_data.csv", "rb") as example_csv_file:
-            csv_file = example_csv_file.read()
-    else:
-        obj = boto3.client("s3").get_object(Bucket=bucket, Key=key)
-        csv_file = obj["Body"].read()
+    csv_file = get_csv_file_binary(bucket, key)
 
     # TODO: change to redis cluster
     # Connect to redis and set pipeline
@@ -49,6 +38,27 @@ def lambda_handler(event, context):
     # TODO: helper function to get the value from redis, remove when production.
     print(client.keys())
     print("done setting key-values with pipeline")
+
+
+def get_csv_file_binary(bucket, key) -> bytes:
+    """   download the CSV file from S3 and parse to iterator of lists """
+    if IS_DEV:
+        # make bytes ourselves, by reading local file directly,
+        # skipping event data altogether.
+        with open("example_data.csv", "rb") as example_csv_file:
+            return example_csv_file.read()
+
+    obj = boto3.client("s3").get_object(Bucket=bucket, Key=key)
+    return obj["Body"].read()
+
+
+def extract_s3_params(event):
+    """ extract the S3 bucket and object key from the event """
+    s3_event_data = event["Records"][0]["s3"]
+    bucket = s3_event_data["bucket"]["name"]
+    key = s3_event_data["object"]["key"]
+
+    return bucket, key
 
 
 if __name__ == "__main__":
